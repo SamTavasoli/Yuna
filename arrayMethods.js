89b33792fa04e6d2976a6cdeb09735651f68da85
ajshiff <ajshiffler@gmail.com>
2020-11-04 16:48:17 -0700

    added array methods file

---
+++ arrayMethods.js
@@ -0,0 +1,21 @@
+//
+let number = 0;
+console.log(typeof(number))
+
+let string = "";
+console.log(typeof(string))
+
+let boolean = false;
+console.log(typeof(boolean))
+
+let object = {};
+console.log(typeof(object))
+
+let array = [];
+console.log(typeof(array))
+
+let nu11 = null;
+console.log(typeof(nu11))
+
+let undef = undefined;
+console.log(typeof(undef))
