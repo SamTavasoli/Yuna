baa889b9f66575526661fd005283a4eb85e28f85
ajshiff <ajshiffler@gmail.com>
2020-11-04 16:57:33 -0700

    added Array.isArray

--- arrayMethods.js
+++ arrayMethods.js
@@ -9,13 +9,21 @@ let boolean = false;
 console.log(typeof(boolean))
 
 let object = {};
-console.log(typeof(object))
+console.log(typeof(object) + Array.isArray(object))
 
 let array = [];
-console.log(typeof(array))
+console.log(typeof(array) + Array.isArray(array))
 
 let nu11 = null;
 console.log(typeof(nu11))
 
 let undef = undefined;
 console.log(typeof(undef))
+
+number
+string
+boolean
+object
+array
+nu11
+undef
