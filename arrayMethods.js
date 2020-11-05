1cd371a97449048898e109726ac37a4d08109be0
ajshiff <ajshiffler@gmail.com>
2020-11-04 17:26:10 -0700

    example of declarative and non declaritive

--- arrayMethods.js
+++ arrayMethods.js
@@ -20,10 +20,41 @@ console.log(typeof(nu11))
 let undef = undefined;
 console.log(typeof(undef))
 
-number
-string
-boolean
-object
-array
-nu11
-undef
+// NOT Declaritive Programming
+console.log("Not very declaritive:")
+var myArray = [1,2,3,4,5,6,7,8,9,0]
+
+var resultArray = [];
+for (var i = 0; i < 10; i++)
+{
+    resultArray.push(myArray[i] + 10) 
+}
+console.log(resultArray);
+
+var numResult = 0;
+for (var i = 0; i < 10; i++)
+{
+    numResult += resultArray[i]
+}
+console.log(numResult)
+
+
+// This is more declaritive:
+console.log("More Declaritive:")
+var numbers = [1,2,3,4,5,6,7,8,9,0];
+
+// Map = 1-to-1 transformation
+var newNumbers = numbers.map(num => num + 10)
+console.log(newNumbers)
+
+// Reduce = many-to-1 transformation
+var newResult = newNumbers.reduce(addArray, 0)
+console.log(newResult)
+
+function addArray (result, number) {
+    console.log(`number: ${number}`)
+    console.log(`result: ${result}`)
+    console.log(`newRes: ${result + number}`)
+    console.log()
+    return result + number
+}
